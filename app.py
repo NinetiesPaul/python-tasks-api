bec5ce6901d707df65bb520f5de40b7985ea7c91
Paul Richard <paulrpm@gmail.com>
2023-10-26 23:25:03 -0300

    added cors package

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from dotenv import dotenv_values
+from flask_cors import CORS
 
 env_var = dotenv_values(".env")
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = env_var['DB_URL']
 mysql = SQLAlchemy(app)
 ma = Marshmallow(app)
