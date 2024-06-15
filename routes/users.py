c5fde6b39fcf8c1d6b284b1aedfa659749055697
Paul Richard <paulrpm@gmail.com>
2024-06-15 19:21:54 -0300

    feat: added call to validator method

--- routes/users.py
+++ routes/users.py
@@ -6,10 +6,11 @@ from functools import wraps
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import app, mysql, env_var
-
 from models.users import Users, user_schema
+from routes import validations
 
 @app.post("/register")
+@validations.validator
 def register():
     user = Users.query.filter_by(email = request.json['email']).first()
     if user:
@@ -17,7 +18,6 @@ def register():
     
     if not re.search("^(.+)@(\\S+)$", request.json['email']):
         return make_response(jsonify({ "msg": "E-mail is invalid", "success": False }), 404)
-
 
     password = generate_password_hash(request.json['password'])
 
@@ -29,6 +29,7 @@ def register():
     return make_response(jsonify({ "data": result, "success": True }), 200)
 
 @app.post("/login")
+@validations.validator
 def login():
     user = Users.query.filter(Users.email == request.json['username']).first()
     if not user:
@@ -40,19 +41,3 @@ def login():
     token = jwt.encode({'email': user.email, 'exp': datetime.datetime.now() + datetime.timedelta(hours=12) }, env_var['SECRET'])
     return make_response(jsonify({ "token": token, "success": True }), 200)
     
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = request.headers.get('Authorization')
-        if not token:
-            return jsonify({'msg': 'token is missing', 'success': False}), 401
-
-        token = token.split()[1]
-        try:
-            data = jwt.decode(token, env_var['SECRET'], algorithms=['HS256'])
-            current_user = Users.query.filter(Users.email == data['email']).one()
-        except:
-            return jsonify({'msg': 'token is invalid or expired', 'success': False}), 401
-
-        return f(current_user, *args, **kwargs)
-    return decorated
