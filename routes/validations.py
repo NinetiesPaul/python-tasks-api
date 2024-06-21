29090385e38558d23ca182243d20a0b35ad8e231
Paul Richard <paulrpm@gmail.com>
2024-06-21 00:30:30 -0300

    feat: improved validations method to validate request inputs

--- routes/validations.py
+++ routes/validations.py
@@ -1,13 +1,40 @@
 import jwt
 from functools import wraps
 from flask import request, jsonify
+import re
 
 from app import env_var
 from models.users import Users
 
-fieldsByRequest = {
-    '/login': [ 'username', 'password' ],
-    '/register': [ 'name', 'email', 'password' ] 
+fieldTypeFormattedName = {
+    'str': 'STRING',
+    'int': 'INTEGER'
+}
+
+pathsToSanitize = {
+    "^/api/task/update/\d+$": "/api/task/update"
+}
+
+requiredFields = {
+    '/register': [ 'name', 'email', 'password' ] ,
+    '/api/task/create': [ 'title', 'description', 'type' ]
+}
+
+fieldByType = {
+    '/register': { 'name': 'str', 'email': 'str', 'password': 'str' },
+    '/api/task/create': { 'title': 'str', 'description': 'str', 'type': 'str' },
+    '/api/task/update': { 'title': 'str', 'description': 'str', 'type': 'str', 'status': 'str' },
+}
+
+fieldNotEmpty = {
+    '/register': [ 'name', 'email', 'password' ],
+    '/api/task/create': [ 'title', 'description', 'type' ],
+    '/api/task/update': [ 'title', 'description', 'type', 'status' ]
+}
+
+fieldIsIn = {
+    '/api/task/create': { 'type': [ 'feature', 'bugfix', 'hotfix' ] },
+    '/api/task/update': { 'type': [ 'feature', 'bugfix', 'hotfix' ], 'status': [ 'closed', 'open', 'in_dev', 'blocked', 'in_qa' ] },
 }
 
 def validator(f):
@@ -16,13 +43,38 @@ def validator(f):
         path = request.path
         requestBody = request.get_json()
 
+        for pathToSanitize in pathsToSanitize.keys():
+            if re.search(pathToSanitize, path):
+                path = pathsToSanitize[pathToSanitize]
+
         messages = []
-        for field in fieldsByRequest[path]:
-            if field not in requestBody:
-                messages.append('INVALID_REQUEST_MISSING_' + field.upper())
+        if path in requiredFields.keys():
+            for field in requiredFields[path]:
+                if field not in requestBody:
+                    messages.append('MISSING_' + field.upper())
+
+        if path in fieldByType.keys():
+            for field in requestBody:
+                if field in fieldByType[path]:
+                    requestFieldType = type(requestBody[field]).__name__ #isinstance(item, str)
+                    expectedFieldType = fieldByType[path][field]
+                    if requestFieldType != expectedFieldType:
+                        messages.append(field.upper() + "_NOT_" + fieldTypeFormattedName[expectedFieldType])
+
+        if path in fieldNotEmpty.keys():
+            for field in requestBody:
+                if field in fieldNotEmpty[path] and type(requestBody[field]).__name__ == 'str':
+                    if len(requestBody[field]) == 0:
+                        messages.append('EMPTY_' + field.upper())
+        
+        if path in fieldIsIn.keys():
+            for field in requestBody:
+                if field in fieldIsIn[path] and requestBody[field] not in fieldIsIn[path][field]:
+                    fieldName = 'INVALID_' + field.upper()
+                    messages.append(fieldName)
         
         if messages:
-            return jsonify({'msg': messages, 'success': False}), 400
+            return jsonify({'message': messages, 'success': False}), 400
 
         return f(*args, **kwargs)
     return decorated
@@ -33,14 +85,14 @@ def token_required(f):
     def decorated(*args, **kwargs):
         token = request.headers.get('Authorization')
         if not token:
-            return jsonify({'msg': 'token is missing', 'success': False}), 401
+            return jsonify({'message': [ "MISSING_TOKEN" ], 'success': False}), 401
 
         token = token.split()[1]
         try:
             data = jwt.decode(token, env_var['SECRET'], algorithms=['HS256'])
             current_user = Users.query.filter(Users.email == data['email']).one()
         except:
-            return jsonify({'msg': 'token is invalid or expired', 'success': False}), 401
+            return jsonify({'message': [ "INVALID_TOKEN" ], 'success': False}), 401
 
         return f(current_user, *args, **kwargs)
     return decorated
