b92ab3d9e2c1cac723f72508ad6f95e2090b9786
Paul Richard <paulrpm@gmail.com>
2025-05-14 01:20:49 -0300

    feat: created test to set up the happy path

---
+++ tests/users/01.set_up/test_set_up.py
@@ -0,0 +1,42 @@
+import pytest
+import requests
+import MySQLdb
+from dotenv import dotenv_values
+
+class TestSetUp:
+    @pytest.fixture()
+    def cleanup(self):
+        env_var = dotenv_values(".env")
+
+        mydb = MySQLdb.connect(
+            host=env_var['DB_TEST_HOST'],
+            user=env_var['DB_TEST_USER'],
+            password=env_var['DB_TEST_PASSWORD'],
+            database=env_var['DB_TEST_DBNAME']
+        )
+
+        mycursor = mydb.cursor()
+
+        mycursor.execute("""
+            SET FOREIGN_KEY_CHECKS = 0;
+
+            TRUNCATE users;
+            TRUNCATE tasks;
+
+            SET FOREIGN_KEY_CHECKS = 1;
+        """)
+        
+        #mydb.commit()
+
+    def test_register_user(self, cleanup):
+        url = "http://localhost:5000/register"
+        new_user = {"name": "Pytest", "email": "user@test", "password": "123456"}
+        response = requests.post(url, json=new_user)
+        assert response.status_code == 200
+        assert response.headers["Content-Type"] == "application/json"
+        
+        responseJson = response.json()["data"]
+        assert isinstance(responseJson, object)
+        assert isinstance(responseJson['id'], int)
+        assert responseJson['name'] == "Pytest"
+        assert responseJson['email'] == "user@test"
