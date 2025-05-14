c5dd79be4c888107cb17e924f4c8e0b1ef72b209
Paul Richard <paulrpm@gmail.com>
2025-05-14 01:20:34 -0300

    feat: handling testing or development env options

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ from flask_cors import CORS
 
 env_var = dotenv_values(".env")
 
+if env_var['ENVIRONMENT'] == 'testing':
+    connection_uri = env_var['DB_URL_TESTING']
+else:
+    connection_uri = env_var['DB_URL']
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = connection_uri
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = env_var['DB_URL']
 mysql = SQLAlchemy(app)
 ma = Marshmallow(app)
 
